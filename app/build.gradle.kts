9400a1f54ea683b2efc55bafda5b3ed69f77e0e2
Jonathan <jonathan@xcy7e.de>
2026-08-29 20:30:20 +0200

    - add fastlane summary.txt
    Signed-off-by: Jonathan <jonathan@xcy7e.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3.1"
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
