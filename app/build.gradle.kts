17fe9d37e73eca2c54bb87dd2da4e5186d63433b
Jonathan <jonathan@xcy7e.de>
2026-08-29 10:59:22 +0200

    v1.2.0
    Signed-off-by: Jonathan <jonathan@xcy7e.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.1.0"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
