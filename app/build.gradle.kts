2214eec6257583918f177d7402c2ab6c153c0905
Jonathan <jonathan@xcy7e.de>
2026-08-31 09:28:19 +0200

    v1.3.3 - add dependenciesInfo config
    Signed-off-by: Jonathan <jonathan@xcy7e.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,13 +14,18 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3.2"
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildFeatures {
         buildConfig = true
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 
     buildTypes {
