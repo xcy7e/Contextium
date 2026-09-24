36af30e4639ca2256d54887addeef7dd06f45212
Jonathan <jonathan@xcy7e.de>
2026-09-24 18:20:35 +0200

    v1.4.0 versionCode 4
    [Add] changelog for version code 4
    
    Signed-off-by: Jonathan <jonathan@xcy7e.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.xcy7e.contextium"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3.6"
+        versionCode = 4
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
